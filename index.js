7b9efc907e997819c2aed29cb6481c4d3d5c0dae
Mayankho <yankhot96@gmail.com>
2019-09-28 23:19:07 -0400

    Changed a few endpints

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
+//libraries 
 const express = require('express');
+
+//Other files
+const db = require('./data/hubs-model.js');
+//This lets you access all the methods inside of the hubs model js file
 
 const server = express();
 
+//Api endpoints
 server.get('/', (request, response) => {
-    response.send('hello world from express');
+    response.send('<h1>hello world from express</h1>');
 });
+
+server.get('/hubs', (req, res) => {
+    db.find()
+    .then(hubs => {
+        console.log(hubs);
+        res.json(hubs)
+    })
+    .catch(err => {
+        res.status(500).json({err: err})
+    });
+})
+//Using the find method on the database file inside of hubs-model.js.
+//Json is a variation of send that explicitly sends json.
+//How is db.find  pulling in an array of hubs.
+
 
 server.get('/now', (request,response) => {
     const now = new Date().toISOString()
@@ -16,7 +37,8 @@ server.listen(4000, () => {
 })
 
 
-//I think thats the wau to get thdat and time: I'm not too sur to b honest
-//
+// express response knows the data type even  if you put the h1 in an string
+// And the status code will come out to 200 if you don explicitly tell it.
 
-//response.send(Date.now)
+//How to access the database, and pull the methods inside the database file
+//Data access layer --> Methods that access the database for you
